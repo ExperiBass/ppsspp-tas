6313635f2a23a449a0d889d44b93dfae531591f8
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2023-02-10 03:09:00 -0500

    a little bit of a upgrade

--- src/main.js
+++ src/main.js
@@ -4,28 +4,30 @@ const config = require('../package.json')
 const ppsspp = new PPSSPP()
 
 /*
-    Inportant requests for TASing
+    # Notes
+    - PPSSPP ignores extra args (probably better to just not send them anyways)
+    # Important requests for TASing
     `input.buttons.send` - used for toggling button pressing - {"buttons": {"button_key": pressed (true|false)}}
     `input.buttons.press` - used for pressing and releasing a button - {"button": button_id, "duration": frames_to_press_for}
     `input.analog.send` - what else? defaults to left stick, but both X and Y are required. - {"x": number (-1 to 1), "y": number (-1 to 1), "stick":"left"(default)|"right"}
 */
 const inputQueue = [
     // freeplay -> venom -> vk -> assegai
-    {button: 'cross', duration: 2, waitBefore: 20},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'cross', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'cross', duration: 2, waitBefore: 10},
-    {button: 'cross', duration: 2, waitBefore: 10},
-    {button: 'cross', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
-    {button: 'down', duration: 2, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'cross', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'cross', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'cross', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'cross', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'cross', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
+    {event: 'input.buttons.press', args: {button: 'down', duration: 2}, waitBefore: 10},
 ]
 // fucking love `nap`
 async function sleep(millis) {
@@ -40,9 +42,9 @@ async function main() {
 
         // go through queue
         for (const input of inputQueue) {
-            console.log(`Sending "${input.button}" for ${input.duration} frames and waiting for ${input.waitBefore}ms`)
+            console.log(`Sending "${input.event}" with args ${JSON.stringify(input.args)} and waiting for ${input.waitBefore}ms`)
             await sleep(input.waitBefore)
-            const hitx = await ppsspp.send({ event: 'input.buttons.press', ...input })
+            const hitx = await ppsspp.send({ event: input.event, ...input.args })
             console.log(hitx)
         }
     } catch(e) {
