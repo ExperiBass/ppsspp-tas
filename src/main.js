2cf05e979a872ab36e3a8bd886b69d588dbeb99f
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2023-02-10 00:39:34 -0500

    some comments

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ const {inputs} = require('./common')
 const config = require('../package.json')
 const ppsspp = new PPSSPP()
 
+/*
+    Inportant requests for TASing
+    `input.buttons.send` - {"buttons": {"button_key": pressed (true|false)}}
+    `input.buttons.press` - {"button": button_id, "duration": frames_to_press_for}
+    `input.analog.send` - {"x": number (-1 to 1), "y": number (-1 to 1), "stick":"left"(default)|"right"}
+*/
+
 async function main() {
     try {
         await ppsspp.autoConnect()
