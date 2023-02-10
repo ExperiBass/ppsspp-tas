13e32995e1322e23570bebf76ba8d8ba40d76045
Gingka/Ginger Pepper <gingkagingerpepper@gmail.com>
2023-02-10 02:30:43 -0500

    ts maybe, not by me tho

--- src/main.js
+++ src/main.js
@@ -5,20 +5,30 @@ const ppsspp = new PPSSPP()
 
 /*
     Inportant requests for TASing
-    `input.buttons.send` - {"buttons": {"button_key": pressed (true|false)}}
-    `input.buttons.press` - {"button": button_id, "duration": frames_to_press_for}
-    `input.analog.send` - {"x": number (-1 to 1), "y": number (-1 to 1), "stick":"left"(default)|"right"}
+    `input.buttons.send` - used for toggling button pressing - {"buttons": {"button_key": pressed (true|false)}}
+    `input.buttons.press` - used for pressing and releasing a button - {"button": button_id, "duration": frames_to_press_for}
+    `input.analog.send` - what else? defaults to left stick, but both X and Y are required. - {"x": number (-1 to 1), "y": number (-1 to 1), "stick":"left"(default)|"right"}
 */
+
 
 async function main() {
     try {
         await ppsspp.autoConnect()
         const handshake = await ppsspp.send({ event: 'version', name: config.name, version: config.version })
         console.log('Connected to', handshake.name, 'version', handshake.version)
+
+        // HIT X (for one frame)
+        const PRESS = {button: 'cross', duration: 2}
+        const hitx = await ppsspp.send({ event: 'input.buttons.press', ...PRESS })
+        console.log(hitx)
     } catch(e) {
         console.error(e)
     } finally {
-		ppsspp.disconnect();
+		ppsspp.disconnect()
 	}
 }
 main()
+
+process.on('SIGINT', () => {
+    ppsspp.disconnect()
+})
